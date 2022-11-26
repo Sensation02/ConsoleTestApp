baab6b58ce8bd9ff91a3f74522f4f5f2b4a329d0
Sensation02 <vasia1415@gmail.com>
2022-11-26 16:13:00 +0200

    Add project files.

---
+++ TestAppBL/Model/Gender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppBL.Model
+{
+    /// <summary>
+    /// Gender
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// gender name
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Creating gender
+        /// </summary>
+        /// <param name="name">gender name</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Gender can't be empty", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ TestAppBL/Model/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppBL.Model
+{
+    internal class User
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DataType BirthDate { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public User(string name, 
+                    Gender gender, 
+                    DateTime birthDate, 
+                    double weight, 
+                    double height)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("User name can't be empty or null", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Gender name can't be null", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900") && birthDate >= DateTime.Now)
+            {
+                
+            }
+            if (weight <= 0.0)
+            {
+
+            }
+            if (height <= 0.0)
+            {
+
+            }
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+    }
+}
